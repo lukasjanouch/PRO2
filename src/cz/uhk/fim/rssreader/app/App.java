74d0ad929fe64a272c4579309268ac5e3bd38e0d
lukasjanouch <lukasjanou8@seznam.cz>
2018-10-08 15:29:43 +0200

    Initial commit

---
+++ src/cz/uhk/fim/rssreader/app/App.java
@@ -0,0 +1,18 @@
+package cz.uhk.fim.rssreader.app;
+
+import cz.uhk.fim.rssreader.gui.MainFrame;
+
+import javax.swing.*;
+
+public class App {
+
+        public static void main (String[]args){
+            SwingUtilities.invokeLater(new Runnable() {
+                @Override
+                public void run() {
+                    new MainFrame().setVisible(true);
+                }
+            });
+        }
+    }
+
